592e3b22de3f450b37c6db0336353554eb91df4d
Ola <olarpringo@gmail.com>
2018-04-28 21:13:56 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Beta.py
+
+**Beta.py** is a finance library for Financial and Investment Formulas.
---
+++ beta/beta.py
@@ -0,0 +1,17 @@
+
+class beta():
+	"""
+		It contains all Finance and Investment formulas 
+		you need for investment decision making.
+
+	""" 
+	def __init__(self):
+		pass
+
+	def ke(self):
+		"""
+			It calculates the cost of capital. 
+			
+		"""
+		pass
+		
