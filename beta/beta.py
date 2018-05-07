6eabfe7b5934dcc9d9822eea30da87eccd5e0c2b
Ola <olarpringo@gmail.com>
2018-05-07 23:11:59 +0100

    added capital asset pricing model

--- beta/beta.py
+++ beta/beta.py
@@ -26,7 +26,6 @@ def cost_of_equity_exdiv(d, Mv):
 	"""
 	ke = d/Mv
 	return ke
-	
 
 
 def cost_of_equity_growth(div, Mv, g):
@@ -96,4 +95,44 @@ def div_growth_rateGm(curr_earn, cap_emp, dt):
 	b = (curr_earn - dt)/curr_earn 
 	g = (roi * b) * 100
 	return g
-	
+
+def capm(rf, beta, rm):
+    """
+    calculate the minimum rate of return using CAPM 
+    (Capital Asset Pricing Model).
+
+    parameters:
+    -----------
+    rf: The risk free rate of retun.
+    beta: The company's beta factor which measures the sensitivity
+        of an investment return to market movement
+    rm: market rate of return
+
+    Notes.
+    ------
+    The CAPM is an alternative method of calculating cost of equity
+    capital for an investment. it is made up of two sides; Risk free
+    rate i.e. the basic rate which all projects must earn if 
+    it is completely free from risk. and the Risk Premium which
+    is gotten after applying the project
+    beta to the difference between market return and risk rate of return.
+
+    Example:
+    --------
+    XYZ limited wants to determine its minimum required rate of return if 
+    the risk free rate is 7%, market rate of return is 10% and the company
+    has a beta factor of 12.
+
+        Solution
+        --------
+        rf = 0.07, beta = 12, rm = 0.10
+        
+        min.required rate of return = 0.07 + 1.2(0.10 - 0.07) = .106 i.e. 10.6%
+
+    """
+    
+    if rf >= 1 or rm >= 1:
+        return "values cannot be greater than or equals 1"
+    else:
+        ke = rf + beta * (rm - rf)
+        return ke	
